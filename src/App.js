55b87648e6ab51951f979647b57a99584147f439
Vegan Programmer <cnrivernene2016@gmail.com>
2023-02-25 03:53:38 +0000

    Hero Section and Menu Tidy Up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import HeroBanner from "./components/HeroBanner";
 import NavBar from "./components/NavBar";
+import PromotionBar from "./components/PromotionBar";
 
 function App() {
 	return (
 		<div className="bg-[url('https://tesla-cdn.thron.com/delivery/public/image/tesla/92cbede0-6d73-4753-a640-7d2acbcd9f3f/bvlatuR/std/1927x4096/Homepage-Model-3-LHD-Mobile')] lg:bg-[url('https://tesla-cdn.thron.com/delivery/public/image/tesla/a269d7b4-87a9-4057-9ac5-2565e56a188e/bvlatuR/std/4096x2560/Homepage-Model-3-Desktop-LHD')] h-screen bg-cover bg-center">
+			<PromotionBar />
 			<NavBar />
+			<HeroBanner />
 		</div>
 	);
 }
---
+++ src/components/HeroBanner.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { FiChevronDown } from "react-icons/fi";
+
+const HeroBanner = () => {
+	return (
+		<div>
+			<div className="absolute inset-x-0 top-[18%] text-center">
+				<h1 className="text-4xl font-bold">Model 3</h1>
+				<p className="p-2 text-sm">
+					Leasing starting at <span>$349/mo</span>
+				</p>
+			</div>
+			<div className="flex flex-col items-center justify-center text-sm lg:flex-row absolute inset-x-0 bottom-[10%]">
+				<a href="https://www.tesla.com/model3/design#overview">
+					<button className="bg-[#393c41] rounded-md w-96 lg:w-64 h-10 lg:mx-4 mt-2 text-white">
+						Custom Order
+					</button>
+				</a>
+				<a href="https://www.tesla.com/drive">
+					<button className="bg-[#f4f4f4] rounded-md w-96 lg:w-64 h-10 lg:mx-4 mt-2 text-[#393c41]">
+						Demo Drive
+					</button>
+				</a>
+			</div>
+			<div className="flex justify-center animate-bounce absolute inset-x-0 bottom-[3%]">
+				<FiChevronDown size={24} />
+			</div>
+		</div>
+	);
+};
+
+export default HeroBanner;
